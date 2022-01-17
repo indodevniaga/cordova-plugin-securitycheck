d6a8d42277075293785b80eab7783192412de6a9
bayu iman <senjuid@gmail.com>
2022-01-17 11:02:42 +0700

    update plugin

--- src/ios/SecurityCheck.swift
+++ src/ios/SecurityCheck.swift
@@ -1,22 +1,50 @@
+import Foundation
 @objc(SecurityCheck) class SecurityCheck : CDVPlugin {
-  @objc(coolMethod:)
-  func coolMethod(_ command: CDVInvokedUrlCommand) {
-    
-    let msg = command.arguments[0] as? String ?? "Error"
-    print(msg)
-   var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR,
-      messageAs: msg
-    )
-if msg.count > 0 {
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
+    @objc(coolMethod:)
+    func coolMethod(_ command: CDVInvokedUrlCommand) {
+        let msg = command.arguments[0] as? String ?? "Error"
+        print(msg)
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR,
+            messageAs: msg)
+        if msg.count > 0 {
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAs: msg)
+        }
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: command.callbackId)
     }
-self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    ) 
-  }
+
+    @objc(isReverseEngineeringDetected:)
+    func isReverseEngineeringDetected(command: CDVInvokedUrlCommand) {
+        let isReversedEngineered = IOSSecuritySuite.amIReverseEngineered()
+        let pluginResult:CDVPluginResult
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: isReversedEngineered)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc(isTampered:)
+    func isTampered(command: CDVInvokedUrlCommand) {
+        var isTampered:Bool
+        let bundle = command.arguments[0] as? String ?? "Error"
+        if IOSSecuritySuite.amITampered([.bundleID(bundle)]).result {
+            isTampered = true
+        }
+        else {
+            isTampered = false
+        }
+        let pluginResult:CDVPluginResult
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: isTampered)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
+
+    @objc(isEmulationDetected:)
+    func isEmulationDetected(command: CDVInvokedUrlCommand) {
+        let isEmulated = IOSSecuritySuite.amIRunInEmulator()
+        let pluginResult:CDVPluginResult
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: isEmulated)
+        self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+    }
 }
--- www/SecurityCheck.js
+++ www/SecurityCheck.js
@@ -6,4 +6,13 @@ var SecurityCheck = function () { }; // This just makes it easier for us to expo
 SecurityCheck.coolMethod = function (arg0, onSuccess, onError) {
     exec(onSuccess, onError, "SecurityCheck", "coolMethod", [arg0]);
 };
+SecurityCheck.isReverseEngineeringDetected = function (onSuccess, onError) {
+    exec(onSuccess, onError, 'SecurityCheck', 'isReverseEngineeringDetected', []);
+};
+SecurityCheck.isTampered = function (bundle, onSuccess, onError) {
+    exec(onSuccess, onError, 'SecurityCheck', 'isTampered', [bundle]);
+};
+SecurityCheck.isEmulationDetected = function (onSuccess, onError) {
+    exec(onSuccess, onError, 'SecurityCheck', 'isEmulationDetected', []);
+};
 module.exports = SecurityCheck;
